a7918322aa8562cbce37db5c4d01be0837197921
GNFinder <goldnuggetfinder9855@gmail.com>
2023-01-12 13:07:29 +0000

    auditing: DigitalNoMadNFTV1.sol main NFT contract - ignoring: gnfNFT.sol & gnfOriginalNFT.sol

--- contracts/digitalNoMadNFTV1.sol
+++ contracts/digitalNoMadNFTV1.sol
@@ -16,13 +16,17 @@ pragma solidity 0.8.17;
 
 // DEPENDENCIES:
 
-/**
-@note https://github.com/openzeppelin/ Documentation is the best way to keep up to date with the latest import libraries
- ***/ 
+
+/// @note https://github.com/openzeppelin/ Documentation is the best way to keep up to date with the latest import libraries
 import "node_modules/@openzeppelin/contracts/utils/Counters.sol"; /// @audit - must add node_modules/@openzeppelin/ as absolute file path for HardHat to read withour errors - @note Remove after before production and after @audit
 import "node_modules/@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol"; /// @audit - (node_modules) appended to front of relevant path as absolute path for HardHat to read withour errors.
 import "node_modules/@openzeppelin/contracts/utils/Strings.sol"; /// @audit - (node_modules) appended to front of relevant file path.
-// import "node_modules/hardhat/console.sol";
+import "node_modules/hardhat/console.sol";
+/**
+@audit - Consider using an interface which will save a considerable amount of gas in storage. 
+Use only the functions called in the contract below, saving the amount
+Of gas used to store "ALL" the (unused) functions from imported contracts above, at compilation time.
+ */
 
 contract DigitalNoMadNFTV1 is ERC721URIStorage {
   /** @note DigitalNoMadNFTV1 is ERC721URIStorage not ERC721 as ERC721 _setTokenURI() was removed in pragma 0.8.0.
@@ -35,8 +39,10 @@ contract DigitalNoMadNFTV1 is ERC721URIStorage {
 
   /**
    @dev Creating a string variable for the NFT svg file - @notice using randomness to generate part of the variable
-   ***/ 
+   */
+   /// @audit DigitalNoMadNFTV1.baseSvg() should be constant - State variables that are not updated following deployment should be declared constant to save gas.
   string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 14px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>"; /// @notice add SVG String HERE
+   /// @note Add the constant attribute to the baseSVG variable that never changes.
 
   /**
   @dev Creating three arrays, each with their own theme of random words.
@@ -52,9 +58,26 @@ contract DigitalNoMadNFTV1 is ERC721URIStorage {
   }
 
   // FUNCTIONS:
+    /**
+    @audit the functions randomMoneyWord, randomTraitWord & randomPlaceWord are using x3 gas 
+    Compared to a singular funtion that grabs all three words from each array.
+    @note below for example:
+    function randomWord() {}
+    if randomMoneyWord == true {
+      return randomTraitWord,
+      else { return randomMoneyWord
+      }
+      if randomTraitWord == true {
+        retturn randomPlaceWord,
+        else { return randomTraitWord
+        }
+    */
+    /** 
+    @audit randomMoneyWord() is never called by the contract so should be declared external.
+    @note Use the external attribute.
+    */ 
 
-    function randomMoneyWord(uint256 tokenId) public view returns (string memory) {
-    // I seed the random generator. More on this in the lesson. 
+  function randomMoneyWord(uint256 tokenId) public view returns (string memory) {
     uint256 rand = random(string(abi.encodePacked("FIRST_WORD", Strings.toString(tokenId))));
     // Squash the # between 0 and the length of the array to avoid going out of bounds.
     rand = rand % moneyWords.length;
@@ -77,11 +100,16 @@ contract DigitalNoMadNFTV1 is ERC721URIStorage {
       return uint256(keccak256(abi.encodePacked(input)));
   }
 
-  function makeADigitalNomadNFT() public { 
+    /** 
+    @audit makeADigitalNomadNFT() Public functions that are never called by the contract should be declared external.
+    @note Use the external attribute.
+    */
+  function makeADigitalNomadNFT() public { /// @audit - Reentrancy in DigitalNoMadNFTV1.makeADigitalNomadNFT()
     /** @audit Better to follow the Checks-Effects-Interactions pattern. 
     Checks are not performed in a public function that makes an external call.
+    @audit Anyone can call this function, essentially minting <= totalSupply of DigitalNoMadNFTV1.
     ***///
-    
+
     uint256 newItemId = _tokenIds.current();
 
     /** 
